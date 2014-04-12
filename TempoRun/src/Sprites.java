4f600e651a2a9678677c79f3bc6b35a95a59ee79
Kevin Pu <kp394@cornell.edu>
2014-04-12 18:14:14 -0400

    Actually added Animation and C-Sprite...I hope

--- TempoRun/src/Sprites.java
+++ TempoRun/src/Sprites.java
@@ -3,11 +3,11 @@ import java.awt.Color;
 
 public class Sprites {
 	
-	private int X;
-	private int Y;
-	private int VX;
-	private int VY;
-	private Color color;
+	protected int X;
+	protected int Y;
+	protected int VX;
+	protected int VY;
+	protected Color color;
 	
 	
 	public Sprites(int x, int y, int vX, int vY, Color C){
@@ -34,9 +34,27 @@ public class Sprites {
 		return VY;
 	}
 	
+	public Color getColor(){
+		return color;
+	}
 	public void setX(int x){
 		this.X = x;
 	}
 	
+	public void setY(int Y){
+		this.Y = Y;
+	}
+	
+	public void setVX(int vx){
+		this.VX = vx;
+	}
+	
+	public void setVY(int vy){
+		this.VY = vy;
+	}
+	
+	public void setColor(Color c){
+		this.color = c;
+	}
 
 }
