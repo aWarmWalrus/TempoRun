093229e1f2f9ae306446ad91c9053cd053f80fb7
Kevin Pu <kp394@cornell.edu>
2014-04-12 17:18:05 -0400

    Added initial files + res

---
+++ TempoRun/src/Sprites.java
@@ -0,0 +1,41 @@
+import java.awt.Color;
+
+
+public class Sprites {
+	
+	private int X;
+	private int Y;
+	private int VX;
+	private int VY;
+	private Color color;
+	
+	
+	public Sprites(int x, int y, int vX, int vY, Color C){
+		this.X = x;
+		this.Y = y;
+		this.VX = vX;
+		this.VY = vY;
+		this.color = C;
+	}
+	
+	public int getX(){
+		return X;
+	}
+	
+	public int getY(){
+		return Y;
+	}
+	
+	public int getVX(){
+		return VX;
+	}
+	
+	public int getVY(){
+		return VY;
+	}
+	
+	public void setX(int x){
+		this.X = x;
+	}
+
+}
